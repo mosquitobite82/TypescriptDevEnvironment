00482f27d5f697f4ad506f51b431ce8b10f66ef7
Johannes Lindgren <mosquitobite82@gmail.com>
2018-01-08 23:08:40 +0100

    Webpack dev server added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,38 @@
-let path = require('path');
-let webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-let rootPath = path.resolve(__dirname);
+const rootPath = path.resolve(__dirname);
+const distFolder = path.join(rootPath, 'dist');
+const sourceFolder = path.join(rootPath, 'src');
 
 let config = {
   devtool: 'inline-source-map',
   entry: [ path.join(rootPath, 'src/index.ts') ],
   output: {
-    path: path.join(rootPath, 'dist/'),
+    path: distFolder,
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
-
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      filename: 'index.html',
+      inject: true
+    }),
+  ],
+  devServer: {
+    contentBase: distFolder,
+    compress: true,
+    port: 9000
   }
 };
 
