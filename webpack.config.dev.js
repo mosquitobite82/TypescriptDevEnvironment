f3cb57c7473b3fd25ffad3dc41f6fcf731a9cc21
Johannes Lindgren <mosquitobite82@gmail.com>
2022-01-18 17:12:45 +0100

    Patching and major updates

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dartSass = require('sass');
 
 const rootPath = path.resolve(__dirname);
 const distFolder = path.join(rootPath, 'dist');
@@ -21,7 +22,18 @@ let config = {
     rules: [
       { test: /\.tsx?$/, use: 'ts-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.scss$/, use: [ 'style-loader' , 'css-loader', 'sass-loader']},
+      { test: /\.scss$/, 
+        use: [
+          'style-loader', 
+          'css-loader', 
+          { 
+            loader: 'sass-loader',
+            options: {
+              implementation: dartSass
+            } 
+          }
+        ]
+      },
     ]
   },
   plugins: [
